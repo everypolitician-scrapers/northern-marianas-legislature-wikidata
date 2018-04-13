83878c6778e61011036821d001cf133bd4e76a1d
Tony Bowden <tony@tmtm.com>
2018-04-13 08:04:16 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,13 @@
 #!/bin/env ruby
-# encoding: utf-8
+# frozen_string_literal: true
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://en.wikipedia.org/wiki/Northern_Mariana_Islands_House_of_Representatives',
-  after: '//span[@id="Members"]',
+names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url:    'https://en.wikipedia.org/wiki/Northern_Mariana_Islands_House_of_Representatives',
+  after:  '//span[@id="Members"]',
   before: '//span[@id="See_also"]',
-  xpath: '//table[.//th[.="Name"]]//td[position() = last() - 1]//a[not(@class="new")]/@title',
-) 
+  xpath:  '//table[.//th[.="Name"]]//td[position() = last() - 1]//a[not(@class="new")]/@title'
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
-
