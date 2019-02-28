c290facd5fc52ccf732aef62f867fd81e0d62511
Tony Bowden <tony@tmtm.com>
2019-02-28 10:37:35 +0000

    Update for changed layout

--- scraper.rb
+++ scraper.rb
@@ -5,9 +5,9 @@ require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.wikipedia_xpath(
   url:    'https://en.wikipedia.org/wiki/Northern_Mariana_Islands_House_of_Representatives',
-  after:  '//span[@id="Members"]',
+  after:  '//span[@id="Members.2C_21st_Legislature"]',
   before: '//span[@id="See_also"]',
-  xpath:  '//table[.//th[.="Name"]]//td[position() = last() - 1]//a[not(@class="new")]/@title'
+  xpath:  '//table[.//th[.="Name"]]//td[position() = last() - 1]//a[not(@class="new")]/@title',
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
